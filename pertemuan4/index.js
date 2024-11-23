6c0f482ee61b0ed3dfe2671777c289ab5d425fde
DELONMYENEKA <delonmyenaka474@gmail.com>
2024-11-23 11:24:37 +0700

    fiks

---
+++ pertemuan4/index.js
@@ -0,0 +1,24 @@
+const variable = "variabel adalah sebuah wadah yang digunakan untuk menampung suatu nilai dalam javascript dapat berupa let(nilai yang dapat diubah) atau const(nilai tetap dan tidal bisa diubah";
+const tipe_data = "tipe data dalam js yaitu bentuk dari sebuah data yang dimasukkan ke dalam variabel dapat berupa string(huruf atau angka yang di apit tanda petik), integer(angka), float(bilangan desimal), boolean(true/false)";
+
+
+const nama = "Delon M Yeneka";
+let panggilan = "Delon";
+const nim = 220117007;
+let umur = "20 tahun";
+const asal_kampus = "Universitas PGRI Sumatera Barat";
+let kelas_yang_diikuti = "javascript";
+const jenis_kelamin = "Laki-laki";
+const L = "Laki-laki";
+const P = "Perempuan";
+
+
+console.log(variable);
+console.log(tipe_data);
+console.log(nama);
+console.log(panggilan);
+console.log(nim);
+console.log(umur);
+console.log(asal_kampus);
+console.log(kelas_yang_diikuti);
+console.log(jenis_kelamin === P);
